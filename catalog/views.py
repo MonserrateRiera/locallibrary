792e5a4d63d550e04abc78e0393515550a14e066
Monserrate Riera Martí <mrieramar@uoc.edu>
2025-05-18 11:40:42 +0200

    Add pagination to BookListView for improved navigation

--- catalog/views.py
+++ catalog/views.py
@@ -28,6 +28,7 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get the context
